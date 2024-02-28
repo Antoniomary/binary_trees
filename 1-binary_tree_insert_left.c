0787c8887bd5294ef8670e7745c3b669535a0154
Antoniomary <amanso.totti@yahoo.com>
2024-02-28 00:58:40 -0800

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -30,6 +30,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	 */
 	node->left = parent->left;
 	parent->left = node;
+	if (node->left)
+		node->left->parent = node;
 
 	node->right = NULL;
 
